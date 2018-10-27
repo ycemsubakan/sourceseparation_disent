0b048704c36db78a29025fec64dd9e9dabef00a6
Massimo Caccia <optimass@cedar5.cedar.computecanada.ca>
2018-10-27 09:54:51 -0700

    meta hyparam

---
+++ meta_hyperparam.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pdb
+import os
+import time
+import sys
+
+
+NN    = ['mlp', 'rnn']
+ATT   = [0 ,1]
+SHARE = [0, 1]
+
+for nn in NN:
+    for att in ATT:
+        for share in SHARE:
+
+            if nn=='rnn' and att:
+                continue
+            
+            command="hyperparam_search.py --nn {} --att {} --share {}".format(nn, att, share)
+            print(command)
+            
+            command = "{} cc_launch.sh {}".format(sys.argv[1], command) 
+
+            os.system(command)
+            time.sleep(2)
+
+
+
+
+
+
+
+
+
+
+
+
